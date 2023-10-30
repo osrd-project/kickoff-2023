654a442ba6465bb0e2d8b8d4ee1120ef2c466894
RomainValls <romain.valls95@gmail.com>
2023-10-30 15:22:55 +0100

    init site

---
+++ kickoff-2023/README.md
@@ -0,0 +1,3 @@
+# kickoff-2023
+
+Landing page for 2023 kickoff communication
---
+++ kickoff-2023/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="app-background"> </div>;
+}
+
+export default App;
