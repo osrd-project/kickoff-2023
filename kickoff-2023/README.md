cb66d0e0d0ea958fd6c287435fc66c11fd589930
Theo Macron <theo.macron0315@gmail.com>
2023-10-30 15:53:54 +0100

    delete useless folder

--- kickoff-2023/README.md
+++
@@ -1,3 +0,0 @@
-# kickoff-2023
-
-Landing page for 2023 kickoff communication
--- kickoff-2023/src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="app-background"> </div>;
-}
-
-export default App;
